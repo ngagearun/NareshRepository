8b0490768dbf400e55c64bab69a64c632895d398
ngagearun <45345193+ngagearun@users.noreply.github.com>
2019-11-24 17:15:21 +0530

    updated file

--- CucumberFrameWork/src/test/java/com/runnerclass/RunCukeTest.java
+++ CucumberFrameWork/src/test/java/com/runnerclass/RunCukeTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions( 
- 			features = "C:/Users/Lenovo/Documents/CucumberFrameWork/src/test/resources/com/features", 
+ 			features = "CucumberFrameWork/src/test/resources/com/features", 
  			glue={"stepDefinations"},
  			plugin = {"pretty","html:target/LoginReport","json:target/cucumber-json/cucumber.json"},
  			tags={"@regressiontesting"},
